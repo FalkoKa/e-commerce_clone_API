95469e1bdd7c05cd7c3e1459167228eab251c49d
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-04-29 17:01:53 +0700

    push and pull to cart

--- controllers/shopping_cart_controller.js
+++ controllers/shopping_cart_controller.js
@@ -2,27 +2,107 @@ const express = require('express');
 const router = express.Router();
 const Cart = require('./../db/Cart');
 const User = require('./../db/User');
+const Product = require('./../db/Product');
 
+// upon signup
 router.post('/new', async (req, res, next) => {
   const { id } = req.body;
-
-  console.log(id);
   try {
     const cart = new Cart({
       user: id,
       ordered: false,
     });
     await cart.save();
-
     const populatedUser = await Cart.findOne({ user: id }).populate('user');
-    console.log(populatedUser);
-
     res.json(populatedUser);
   } catch (error) {
     console.log(error);
   }
 });
 
-router.get('/', async (req, res, next) => {});
+// get single cart from a user
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const cart = await Cart.findOne({ user: id })
+      .populate({
+        path: 'items',
+        populate: { path: 'item' },
+      })
+      .populate('user');
+    res.json(cart);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// post new items to cart
+router.post('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { itemId } = req.body;
+
+  try {
+    const cart = await Cart.updateOne(
+      { user: id },
+      { $push: { items: { quantity: 1, item: itemId } } }
+    );
+
+    const updatedCart = await Cart.findOne({ user: id })
+      .populate({
+        path: 'items',
+        populate: { path: 'item' },
+      })
+      .populate('user');
+    res.json(updatedCart);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// update cart
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { quantity, itemID } = req.body;
+  try {
+    const cart = await Cart.updateOne(
+      { user: id, 'items.item': itemID },
+      {
+        $set: {
+          'items.$quantity': quantity,
+          'items.$item': itemID,
+        },
+      }
+    );
+    console.log(cart);
+
+    const updatedCart = await Cart.findOne({ user: id })
+      .populate({
+        path: 'items',
+        populate: { path: 'item' },
+      })
+      .populate('user');
+    res.json(updatedCart);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// delete item from cart
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { itemId } = req.body;
+
+  try {
+    const cart = await Cart.updateOne(
+      { user: id },
+      { $pull: { items: { item: itemId } } }
+    );
+
+    const updatedCart = await Cart.findOne({ user: id });
+    res.json(updatedCart);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
